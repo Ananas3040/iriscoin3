8d796f007f562bae2f55ae2941518304aaefe22f
Марченко Андрей <48173468+MrSmitix@users.noreply.github.com>
2021-01-08 03:54:16 +0500

    Первое обновление
    Теперь токены будут браться из переменной окружения

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Подключаем apscheduler для создания cron-задачи
 from apscheduler.schedulers.blocking import BlockingScheduler
 # Импортируем значение задержки из настроек
-from settings import DELAY
+from settings import TOKENS, DELAY
 # Импортируем функцию самого бота
 from bot import main
 
@@ -11,5 +11,5 @@ scheduler.add_job(main, 'interval', seconds=DELAY)
 
 
 if __name__ == '__main__':
-    print('Запускаем задачу...')
+    print(f'Запускаем задачу... Загружено {len(TOKENS)} токенов!')
     scheduler.start()
